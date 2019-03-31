db4b703dde28e86e9ed7abff2708df438ba22c09
xuhang <xuhang1128@gmail.com>
2019-03-31 16:47:06 +0800

    add spring data redis

--- spring-boot-crud/src/main/java/com/baeldung/crud/controllers/UserController.java
+++ spring-boot-crud/src/main/java/com/baeldung/crud/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.baeldung.crud.controllers;
 
-import javax.validation.Valid;
-
+import com.baeldung.crud.entities.User;
+import com.baeldung.crud.repositories.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,12 +15,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.baeldung.crud.entities.User;
-import com.baeldung.crud.repositories.UserRepository;
+import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    // inject the actual template
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    // inject the template as ListOperations
+//    @Resource(name="redisTemplate")
+//    private ListOperations<String, String> listOps;
+//
+//    @Autowired
+//    private StringRedisTemplate redisTemplate;
 
     private final UserRepository userRepository;
 
@@ -33,12 +47,14 @@ public class UserController {
     
     @PostMapping("/adduser")
     public String addUser(@Valid User user, BindingResult result, Model model) {
+
         logger.debug("user added:" + model);
         if (result.hasErrors()) {
             return "add-user";
         }
         
         userRepository.save(user);
+        redisTemplate.opsForValue().set("user"+user.getId() , user.getEmail());
         model.addAttribute("users", userRepository.findAll());
         return "index";
     }
